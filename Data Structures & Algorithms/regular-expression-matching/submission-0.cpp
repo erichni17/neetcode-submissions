5e61e9eafdec9b05530913b8ab1e5080ea750b35
Eric Ni <nierh2007@gmail.com>
2026-06-04 15:08:05 -0400

    Add: regular-expression-matching - submission-0

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.length(); 
+        int m = p.length(); 
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;  
+
+        //recurrence relation: if p[j] == *, dp[i][j] = dp[i-1][j] else if (p[j] == '.' || s[i] == p[j]) && dp[i-1][j-1], dp[i][j] = true; 
+
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (p[j-1] == '*') {
+                    dp[i][j] = dp[i][j-2] || (i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.')) && dp[i-1][j]; 
+                }
+                else if (i > 0 && (p[j-1] == '.' || s[i-1] == p[j-1])) {
+                    dp[i][j] = dp[i-1][j-1]; 
+                }
+            }
+        }
+
+        return dp[n][m]; 
+    }
+};
